7f9c5331dac04953f3220803d9d0b9cb151133cb
nafeng <fovkang@163.com>
2019-06-04 21:17:43 +0800

    修改Test01

--- src/com/itheima/Test01.java
+++ src/com/itheima/Test01.java
@@ -41,6 +41,7 @@ public class Test01 {
         for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i));
         }
+        //打印集合
         System.out.println(list);
     }
 
